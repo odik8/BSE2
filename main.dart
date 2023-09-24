405d8cf6d40d94ca74a17c2cbc171493694d8c05
Odyssey Meltonyan <odissey.meltonyan@gmail.com>
2023-09-24 12:18:57 +0300

    Commit 4

--- main.dart
+++ main.dart
@@ -1,10 +1,27 @@
 import 'dart:io';
 
 void main() {
-  print('o/');
+  print('<o/');
   print(heloUser('Ilon Mask'));
 }
 
 heloUser(String name) {
-  return 'Hello $name';
+  // Получаем текущее время
+  DateTime now = DateTime.now();
+  // Получаем час из текущего времени
+  int hour = now.hour;
+
+  // Определяем приветствие на основе времени суток
+  String greeting;
+  if (hour < 12) {
+    greeting = 'Доброе утро!';
+  } else if (hour < 18) {
+    greeting = 'Добрый день!';
+  } else {
+    greeting = 'Добрый вечер!';
+  }
+
+  // Выводим приветствие на экран
+  return greeting + ' ' + name;
+  ;
 }
